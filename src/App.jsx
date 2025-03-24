8222a6b3ce16f7b2afdb10e24f0df2a4dad2298c
Akhilendra Pratap Singh Chandel <akhilendra.chandel@techcarrel.com>
2025-03-24 22:31:23 +0530

    Add Navbar component and integrate it into App; update CSS to include daisyUI plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from "./components/Navbar";
+
 const App = () => {
   return (
-    <div className="bg-blue-100 font-bold text-6xl text-center h-screen content-center hover:text-12xl transition:all">
-      App
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 };
 
