32b5de935cdf940436c014a02614819acae132b6
Akhilendra Pratap Singh Chandel <akhilendra.chandel@techcarrel.com>
2025-03-24 22:16:04 +0530

    Initialize React app with Vite and Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="bg-blue-100 font-bold text-6xl text-center h-screen content-center hover:text-12xl transition:all">
+      App
+    </div>
+  );
+};
+
+export default App;
